cbf006aeec25c15453746b4e8d6ea2b2be9c520b
levien4 <levienvdw@gmail.com>
2016-02-24 17:33:45 +0100

    adding read more button

--- function.js
+++ function.js
@@ -6,10 +6,18 @@ $(window).scroll(function(){
     'background-position': 'center -'+ (wScroll /8) +'px'
   });
 
-  //$('.window').css({
-  //  'opacity': ''+ (wScroll / $(window).height()) +''
-  //});
+  $('.more').css({
+    'opacity': ''+ (($(window).height() - wScroll * 3) / 1000) +''
+  });
+
   $('.window').css({
     'background-position': 'center -'+ (wScroll /8) +'px'
   });
+});
+
+$(function() {
+    $(".read").on("click", function() {
+        $("body").animate({"scrollTop": window.scrollY + (window.innerHeight - 500)}, 2000);
+        return false;
+    });
 });
