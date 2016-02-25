f93770b32b72e50b23b87fd84d688ec20da7f8b5
levien4 <levienvdw@gmail.com>
2016-02-25 15:03:18 +0100

    mobile optimizations

--- function.js
+++ function.js
@@ -1,23 +1,29 @@
-$(window).scroll(function(){
+if ( $(window).width() > 1000) {
+  $(window).scroll(function(){
 
-  var wScroll = $(this).scrollTop();
+    var wScroll = $(this).scrollTop();
 
-  $('header').css({
-    'background-position': 'center -'+ (wScroll /8) +'px'
-  });
-
-  $('.more').css({
-    'opacity': ''+ (($(window).height() - wScroll * 3) / 1000) +''
-  });
-
-  $('.window').css({
-    'background-position': 'center -'+ (wScroll /8) +'px'
-  });
-});
-
-$(function() {
-    $(".read").on("click", function() {
-        $("body").animate({"scrollTop": window.scrollY + (window.innerHeight - 500)}, 2000);
-        return false;
+    $('header').css({
+      'background-position': 'center -'+ (wScroll /8) +'px'
     });
-});
+
+    $('.more').css({
+      'opacity': ''+ (($(window).height() - wScroll * 3) / 1000) +''
+    });
+
+    $('.window').css({
+      'background-position': 'center -'+ (wScroll /8) +'px'
+    });
+  });
+
+  $(function() {
+      $(".read").on("click", function() {
+          $("body").animate({"scrollTop": window.scrollY + (window.innerHeight - 500)}, 2000);
+          return false;
+      });
+  });
+  //Add your javascript for large screens here
+}
+else {
+  //Add your javascript for small screens here
+}
