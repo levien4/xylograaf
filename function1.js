84267a035c5a0579aa7f1e9c3cc0c70ef8b1ce41
levien4 <levienvdw@gmail.com>
2016-03-13 17:33:36 +0100

    kleine veranderingen aan editor

--- function1.js
+++ function1.js
@@ -8,9 +8,25 @@ $(document).ready(function(){
     var input5 = parseInt($("#saturate").val());
     var input6 = parseInt($("#invert").val());
     var input7 = parseInt($("#blur").val());
-    console.log(input);
+    //console.log(input);
     $('img').css({
-      '-webkit-filter': 'contrast('+ input/10 +') brightness(' + input1*2 + '%) grayscale(' + input2 +'%) hue-rotate('+ input3*1.8 +'deg) sepia('+ input4 +'%) saturate('+ input5*10 +'%) invert('+ input6 +'%) blur('+ input7/10 +'px)'
+      '-webkit-filter': 'contrast('+ input/10 +') brightness(' + input1*2 + '%) grayscale(' + input2 +'%) hue-rotate('+ input3*3.6 +'deg) sepia('+ input4 +'%) saturate('+ input5*10 +'%) invert('+ input6 +'%) blur('+ input7/10 +'px)'
     });
+    document.getElementById('c').innerHTML=
+    ""+ input +"";
+    document.getElementById('b').innerHTML=
+    ""+ input1 +"";
+    document.getElementById('g').innerHTML=
+    ""+ input2 +"";
+    document.getElementById('h').innerHTML=
+    ""+ input3 +"";
+    document.getElementById('s').innerHTML=
+    ""+ input4 +"";
+    document.getElementById('sa').innerHTML=
+    ""+ input5 +"";
+    document.getElementById('i').innerHTML=
+    ""+ input6 +"";
+    document.getElementById('bl').innerHTML=
+    ""+ input7 +"";
   });
 });
