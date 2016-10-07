3e29098b4df4207c1ed2c6861ce2399761229046
levien4 <levienvdw@gmail.com>
2016-10-07 21:52:16 +0200

    material levien jouke update 1.1.2

---
+++ ghana/inschrijven.php
@@ -0,0 +1,22 @@
+<html>
+<head>
+  <meta charset="UTF-8">
+  <title>Scheurkalender</title>
+  <link href='https://fonts.googleapis.com/css?family=Roboto' rel='stylesheet' type='text/css'>
+  <link rel="stylesheet" type="text/css" href="style.css">
+  <link rel="icon" type="image/svg" sizes="any" href="https://www.worldservants.nl/sites/all/themes/worldservants/images/world-servants/logo.svg">
+</head>
+<body bgcolor="#42616f">
+  <div class="background">
+  </div>
+    <div class="content">
+      <div class="header">
+        <a href="index.html"><img src="wslogo.png"></a>
+        <h1>Zwolle</h1>
+        <form>
+
+        </form>
+    </div>
+</div>
+</body>
+</html>
