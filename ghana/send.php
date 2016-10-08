1a64a102675914df6630f84929b1d526f89c0cb9
levien4 <levienvdw@gmail.com>
2016-10-08 21:57:51 +0200

    name database changed

--- ghana/send.php
+++ ghana/send.php
@@ -6,7 +6,7 @@ $name = mysql_real_escape_string($_POST['naam']);
 $phone = mysql_real_escape_string($_POST['telefoon']);
 $email = mysql_real_escape_string($_POST['email']);
 $date = mysql_real_escape_string($_POST['datum']);
-$order = "INSERT INTO data_dates (id, name, phone, email, datum) VALUES(NULL, '$name', '$phone', '$email', '$date')";
+$order = "INSERT INTO dates (id, name, phone, email, datum) VALUES(NULL, '$name', '$phone', '$email', '$date')";
 
 $result = mysql_query($order);
 if($result){
