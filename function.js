8dce8cfeced489e49dcbdcf6a8576611e6cb7c3b
levien4 <levienvdw@gmail.com>
2017-03-22 19:28:35 +0100

    favicon toegevoegd

--- function.js
+++ function.js
@@ -1,38 +1,33 @@
-if ( $(window).width() > 1000) {
-  $(window).scroll(function(){
-
-    var wScroll = $(this).scrollTop();
-
-    $('header').css({
-      'background-position': 'center -'+ (wScroll /8) +'px'
-    });
-
-    $('.more').css({
-      'opacity': ''+ (($(window).height() - wScroll * 3) / 1000) +''
-    });
-
-    $('.window').css({
-      'background-position': 'center -'+ (wScroll /8) +'px'
-    });
-  });
-
-  $(function() {
-      $(".read").on("click", function() {
-          $("body").animate({"scrollTop": window.scrollY + (window.innerHeight - 500)}, 2000);
-          return false;
-      });
-  });
-  //Add your javascript for large screens here
-}
-else {
-  $('#burger').click(function(){
-    console.log(1);
-    $('ul').toggleClass("show");
-  });
-}
 $(document).ready(function(){
-  $('#burger').click(function(){
-    console.log(1);
-    $('ul').toggleClass("show");
-  });
+  if ( $(window).width() > 1000) {
+    $(window).scroll(function(){
+
+      var wScroll = $(this).scrollTop();
+
+      $('header').css({
+        'background-position': 'center -'+ (wScroll /8) +'px'
+      });
+
+      $('.more').css({
+        'opacity': ''+ (($(window).height() - wScroll * 3) / 1000) +''
+      });
+
+      $('.window').css({
+        'background-position': 'center -'+ (wScroll /8) +'px'
+      });
+    });
+
+    $(function() {
+        $(".read").on("click", function() {
+            $("body").animate({"scrollTop": window.scrollY + (window.innerHeight - 500)}, 2000);
+            return false;
+        });
+    });
+    //Add your javascript for large screens here
+  } else {
+    $('#burger').click(function(){
+      console.log(1);
+      $('ul').toggleClass("show");
+    });
+  }
 })
