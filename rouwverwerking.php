aefbb30fe557aa7154bb9e24b5717f4d8efaf16f
levien4 <levienvdw@gmail.com>
2017-04-28 14:39:33 +0200

    resized images

--- rouwverwerking.php
+++ rouwverwerking.php
@@ -2,7 +2,7 @@
 <html>
 <head>
   <meta charset="UTF-8">
-  <title>Gedragsproblemen</title>
+  <title>Rouwverwerking</title>
   <meta name="viewport" content="width=500px, initial-scale=1.0">
   <link rel="stylesheet" type="text/css" href="style.css">
   <link rel="icon" type="image/x-icon" href="favicon.png">
@@ -38,12 +38,12 @@
         <div class="header">
           <a href="index.php"><div id="logo"><img src="logo.jpg"></div></a>
           <img src="1.jpg" width="30%">
-          <img src="6.jpg" width="30%">
+          <img src="5.jpg" width="13.3%">
           <img src="3.jpg" width="30%">
-          <h1>-gedragsproblemen-</h1>
+          <h1>-Rouwverwerking-</h1>
           <div class="text">
-            Kids' Skills is een methode waarmee kinderen op een positieve, constructieve manier problemen overwinnen door nieuwe vaardigheden te leren. De methode leent zich voor het oplossen van de doodgewone, alledaagse problemen die voorkomen bij het opvoeden van kinderen, maar ook voor de ernstigere problematiek.<p>
-Orthopedagogische praktijk De Xylograaf biedt vanuit de methode Kids’ Skills ondersteuning aan kinderen en ouders die vragen hebben op het gebied van opgroeien en opvoeden.
+            In Nederland verliezen jaarlijks duizenden kinderen een eerstegraads familielid (een ouder, broer of zus). Uit onderzoek is gebleken dat deze kinderen een verhoogde kans hebben op het ontwikkelen van problemen. Zo heeft 20 procent twee jaar na een verlies dusdanig ernstige psychosociale problemen dat professionele hulp noodzakelijk is. Vroegtijdige onderkenning en behandeling van kinderen die mogelijk vastlopen in de verwerking van verlies is daarom wenselijk.<p>
+            Je kunt met zorgen over rouwverwerking terecht bij orthopedagogische praktijk De Xylograaf. Ik ga samen met het kind kijken hoe het de rouw verwerkt en of het daar hulp bij nodig heeft.
           </div>
       </div>
   </div>
