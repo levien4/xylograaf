96feb3ca027f900f78b50033d93d44ea2011a859
levien4 <levienvdw@gmail.com>
2017-09-10 14:55:59 +0200

    onder constructie

--- constructie.php
+++ constructie.php
@@ -2,7 +2,7 @@
 <html>
 <head>
   <meta charset="UTF-8">
-  <title>Rouwverwerking</title>
+  <title>de Xylograaf</title>
   <meta name="viewport" content="width=device-width">
   <link rel="stylesheet" type="text/css" href="style.css">
   <link rel="icon" type="image/x-icon" href="favicon.png">
@@ -38,12 +38,11 @@
         <div class="header">
           <a href="index.php"><div id="logo"><img src="logo.jpg"></div></a>
           <img src="1.jpg" width="30%">
-          <img src="5.jpg" width="13.3%">
+          <img src="2.jpg" width="30%">
           <img src="3.jpg" width="30%">
-          <h1>-Rouwverwerking-</h1>
+          <h1>-Onder Constructie-</h1>
           <div class="text">
-            In Nederland verliezen jaarlijks duizenden kinderen een eerstegraads familielid (een ouder, broer of zus). Uit onderzoek is gebleken dat deze kinderen een verhoogde kans hebben op het ontwikkelen van problemen. Zo heeft 20 procent twee jaar na een verlies dusdanig ernstige psychosociale problemen dat professionele hulp noodzakelijk is. Vroegtijdige onderkenning en behandeling van kinderen die mogelijk vastlopen in de verwerking van verlies is daarom wenselijk.<p>
-            Je kunt met zorgen over rouwverwerking terecht bij orthopedagogische praktijk De Xylograaf. Ik ga samen met het kind kijken hoe het de rouw verwerkt en of het daar hulp bij nodig heeft.
+            Deze pagina is nog niet helemaal voltooid. Komt u op een later moment nog een keer terug. Excuses voor het ongemak.
           </div>
       </div>
   </div>
--- gedragsproblemen.php
+++ gedragsproblemen.php
@@ -19,18 +19,18 @@
             <li><a href="dyslexie.php">Dyslexie</a></li>
             <li><a href="gedragsproblemen.php">Gedragsproblemen</a></li>
             <li><a href="rouwverwerking.php">Rouwverwerking</a></li>
-            <li><a href="trainingen.php">Trainingen</a></li>
+            <li><a href="constructie.php">Trainingen</a></li>
           </ul>
         </li>
         <li>Werkwijze
           <ul>
             <li><a href="intake.php">Intake</a></li>
             <li><a href="onderzoek.php">Onderzoek</a></li>
-            <li><a href="advies.php">Advies</a></li>
+            <li><a href="constructie.php">Advies</a></li>
             <li><a href="begeleiding.php">Begeleiding</a></li>
-            <li><a href="behandeling.php">Behandeling</a></li>
-            <li><a href="">Behandelingsovereenkomst</a></li>
-            <li><a href="bekostiging.php">Bekostiging</a></li>
+            <li><a href="constructie.php">Behandeling</a></li>
+            <li><a href="constructie.php">Behandelingsovereenkomst</a></li>
+            <li><a href="constructie.php">Bekostiging</a></li>
           </ul>
         </li>
         <li><a href="contact.php">Contact</a></li>
