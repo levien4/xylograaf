57a62632b277b784de1a1e64a7aec20ed895b6a8
xylograaf <48626247+xylograaf@users.noreply.github.com>
2020-04-27 20:02:36 +0200

    verhalen
    gedicht

--- verhalen.php
+++ verhalen.php
@@ -21,14 +21,13 @@
           <img src="3.jpg" width="30%">
           <h1>-verhalen-</h1>
           <div class="text">
-            Hieronder volgen een aantal citaten uit boeken over een houtsnijder. Aanraders voor jong en oud.
-
-            <p>
+            Hieronder volgen een aantal citaten uit boeken over een houtsnijder. Aanraders voor jong en oud.<p>
             <h2>Michiel en de houtsnijder, Gerard van Midden en Richard Feld</h2>Het is vlak voor Kerst. Michiel dwaalt door de stad. Opeens staat hij voor de winkel van Johan, de houtsnijder. Hij maakt houten poppen. Tingeling. De winkeldeur gaat open. Joham kijkt op. Hij legt het scherpe mes neer en klopt de houtkrullen van zijn jas. 'Dag jongen,' zegt hij. 'Wil je even rondkijken?' Michiel knikt. De poppen zijn  net echt. Voorzichtig strijkt hij een houten prinses over het zilveren haar. 'Vind je ze mooi?' Michiel knikt en fluistert: 'Heel mooi!'<p>
             <h2>De houtsnijder, Martine Jonker</h2>De illustraties door de Vlaamse tekenaar Luc Vincent maken dit boek tot een prachtig lees- en voorleesboek voor iedereen van 8-88 jaar.<p>
             <h2>De Nerflanders, Max Lucado</h2>(…) De Nerflanders waren kleine mensen van hout en waren gemaakt door Eli, een houtsnijder. Eli’s werkplaats lag bovenop een heuvel, zodat hij het hele dorp kon zien. (...) Niemand is zoals jij is een hartverwarmend verhaal met een belangrijke boodschap voor kinderen én volwassenen: het maakt niet uit wat andere mensen van hen denken.
-
-          </div>
+<p>En als afsluiting nog een gedicht van Michelangelo:<p>
+  De grootste kunstenaar kan niet verzinnen<p>wat niet vooraf al in de steen bestaat.<p>Maar als zijn hand niet met zijn geest meegaat,<p>zal hij het nooit van het ruwe marmer winnen.
+            </div>
       </div>
   </div>
   <div class="footer">
