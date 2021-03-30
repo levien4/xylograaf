87e46da6cefab56aeca4cef82d0188e446cc9cd7
xylograaf <48626247+xylograaf@users.noreply.github.com>
2021-03-30 12:42:43 +0200

    geactualiseerd

--- contact.php
+++ contact.php
@@ -23,9 +23,9 @@
             Na de studie Orthopedagogiek ben ik gestart met onderzoek en behandeling van kinderen met dyslexie. Ik ben opgeleid tot dyslexieprofessional. Voor hulp bij gedragsproblemen en sociaal-emotionele problemen, heb ik gecertificeerde cursussen gevolgd. Ik ben in opleiding tot cognitief gedragstherapeut.<p>Sinds februari 2014 ben ik als orthopedagoog werkzaam. Mijn werkzaamheden bestaan uit onderzoeken uitvoeren en advies geven. Ook begeleid ik kinderen en jongeren met uiteenlopende zorgen. Ik heb veel ervaring opgedaan met het begeleiden van kinderen met leerproblemen. Als er sprake is van (faal)angst, een negatief zelfbeeld of gedragsproblemen gaan we in de begeleiding op zoek naar mogelijkheden voor het verminderen of oplossen hiervan. Bijdragen aan een kindertijd waarin de zorgen het kind en de ouders niet boven het hoofd groeien, is mijn drijfveer. Het doel is dat het kind of de jongere weer zoveel mogelijk op eigen kracht vooruit kan.
 <p><em>Er is nog volop mogelijkheid te starten met een begeleidingstraject in Zwolle (noord). Tot aan de zomervakantie wordt je kind dan wekelijks geholpen met zijn of haar leerproblemen of bied ik begeleiding bij het verminderen  van de zorgen die jij en je kind op dit moment ervaren.</em><br>
 <p>Mijn werkdagen op de praktijk zijn maandag en dinsdag van 9.00 tot 17.00 uur.
-Marike van der Woerd, orthopedagoog en eigenaar van de Xylograaf.<p>
-U kunt zich van maandag tot en met vrijdag telefonisch aanmelden van 09.00 tot 17.00 uur. Wanneer de praktijk niet bereikbaar is kunt u het antwoordapparaat inspreken, u wordt dan zo spoedig mogelijk teruggebeld. Tevens kunt u zich per email aanmelden. U kunt zonder verwijzing een afspraak maken.<p>
 <p>
+<p>Van maandag tot en met vrijdag is telefonisch aanmelden mogelijk van 09.00 tot 17.00 uur. Wanneer de praktijk niet bereikbaar is, spreek je het antwoordapparaat in. Een bericht via Whatsapp is ook mogelijk, vermeld dan in het bericht je naam. Er wordt dan zo snel mogelijk teruggebeld. Contact per e-mail is ook altijd mogelijk.<p>
+<p>Marike van der Woerd, orthopedagoog en eigenaar van de Xylograaf.<p>
 
             <img src="site.png" width="20px"><a href="www.xylograaf.nl"> www.xylograaf.nl</a><br></>
             <img src="email.png" width="20px"><a href="mailto:xylograaf@ziggo.nl"> xylograaf@ziggo.nl</a><br>
